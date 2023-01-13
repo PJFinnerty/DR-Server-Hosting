eae757f5a268546a03b1bbddcf218ab6b004a2f9
PJFinnerty <60487259+PJFinnerty@users.noreply.github.com>
2023-01-13 22:45:26 +0000

    Update simpleserver.py

--- simpleserver.py
+++ simpleserver.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='', static_folder='.')
 
 @app.route('/', methods=['GET'])
 def printHello():
-    return "Hello"
+    return "Hello, are ya well?"
 
 if __name__ == '__main__' :
     app.run(debug= True)
